c29c0e1fe09f8c2961a534f744583faccc1257cc
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-24 02:13:13 +0530

    size fixed

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const board = document.querySelector('.board')
-const blockWidth = 30
-const blockHeight = 30
+const blockWidth = 40
+const blockHeight = 40
 
 const cols = Math.floor(board.clientWidth / blockWidth)
 const rows = Math.floor(board.clientHeight / blockHeight)
 
-for(let i = 0; i < rows * cols; i++){
-    const block = document.createElement('div')
-    block.classList.add('block')
-    board.appendChild(block)
+const blocks = []
+const snake = [
+    {
+        x: 1, y: 3
+    }, 
+
+    {
+        x: 1, y: 4
+    }, 
+
+    {
+        x: 1, y: 5
+    }
+]
+
+for(let row = 0; row < rows; row++){
+    for(let col = 0; col < cols; col++){
+        const block = document.createElement('div')
+        block.classList.add('block')
+        board.appendChild(block)
+        blocks
+    }
 }
