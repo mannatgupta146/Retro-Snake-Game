0e99b0f0bcbae480c915686b20b693053da86d86
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-25 19:32:00 +0530

    working

--- script.js
+++ script.js
@@ -20,11 +20,25 @@ const snake = [
     }
 ]
 
+let direction = ''
+
 for(let row = 0; row < rows; row++){
     for(let col = 0; col < cols; col++){
         const block = document.createElement('div')
         block.classList.add('block')
         board.appendChild(block)
-        blocks
+        blocks[`${row}, ${col}`] = block
     }
+}
+
+// let c1 = Math.floor(Math.random()*256)
+// let c2 = Math.floor(Math.random()*256)
+// let c3 = Math.floor(Math.random()*256)
+
+// document.documentElement.style.setProperty('--bg-fill-color', `rgb(${c1}, ${c2}, ${c3})`)
+
+function render(){
+    snake.forEach(segment => {
+        blocks[`${segment.x}, ${segment.y}`].classList.add('fill')
+    })
 }
