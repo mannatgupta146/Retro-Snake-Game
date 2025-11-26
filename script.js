df49d71594666d13acbcdde06feeb8728a680a97
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-26 15:45:39 +0530

    core logic done

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const blockHeight = 40
 const cols = Math.floor(board.clientWidth / blockWidth)
 const rows = Math.floor(board.clientHeight / blockHeight)
 
+let intervalId = null
+
 const blocks = []
 const snake = [
     {
@@ -20,7 +22,9 @@ const snake = [
     }
 ]
 
-let direction = ''
+let food = {x: Math.floor(Math.random()*rows), y: Math.floor(Math.random()*cols)}
+
+let direction = 'right'
 
 for(let row = 0; row < rows; row++){
     for(let col = 0; col < cols; col++){
@@ -38,7 +42,64 @@ for(let row = 0; row < rows; row++){
 // document.documentElement.style.setProperty('--bg-fill-color', `rgb(${c1}, ${c2}, ${c3})`)
 
 function render(){
+
+    let head = null
+
+    blocks[`${food.x}, ${food.y}`].classList.add('food')
+
+    if(direction === 'left'){
+        head = {x: snake[0].x, y: snake[0].y - 1}
+    }
+    else if(direction === 'right'){
+        head = {x: snake[0].x, y: snake[0].y + 1}
+    }
+    else if(direction === 'down'){
+        head = {x: snake[0].x + 1, y: snake[0].y}
+    }
+    else if(direction === 'up'){
+        head = {x: snake[0].x - 1, y: snake[0].y}
+    }
+
+    if(head.x<0 || head.x>=rows || head.y<0 || head.y>=cols){
+        alert('Game Over')
+        clearInterval(intervalId)
+    }
+
+    if(head.x === food.x && head.y === food.y){
+        blocks[`${food.x}, ${food.y}`].classList.remove('food')
+        food = {x: Math.floor(Math.random()*rows), y: Math.floor(Math.random()*cols)}
+        blocks[`${food.x}, ${food.y}`].classList.add('food')
+        snake.unshift(head)
+    }
+
+    snake.forEach(segment => {
+        blocks[`${segment.x}, ${segment.y}`].classList.remove('fill')
+    })
+
+    snake.unshift(head)
+    snake.pop()
+
     snake.forEach(segment => {
         blocks[`${segment.x}, ${segment.y}`].classList.add('fill')
     })
 }
+
+intervalId = setInterval(() =>{
+    render()
+}, 300)
+
+
+addEventListener('keydown', (event) =>{
+    if(event.key === 'ArrowUp'){
+        direction = 'up'
+    }
+    else if(event.key === 'ArrowDown'){
+        direction = 'down'
+    }
+    else if(event.key === 'ArrowLeft'){
+        direction = 'left'
+    }
+    else if(event.key === 'ArrowRight'){
+        direction = 'right'
+    }
+})
